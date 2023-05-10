10ad92c0068b0b5d67328edbd13698b93116b006
“Gilbert <>
2023-05-09 20:48:43 -0700

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Function to query number_of_subscribers 
-"""
+"""Function to query the number_of_subscribers"""
 
 import requests
 
@@ -10,8 +8,8 @@ def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    results = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
                      headers={'User-Agent': '0x16-api_advanced:project:\
 v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = result.get("data", {}).get("subscribers", 0)
+    subs = r.get("data", {}).get("subscribers", 0)
     return subs
