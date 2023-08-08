0affebb57bbfd2ba6a1edf966a0bef6e332a0806
Gilbert Segakweng <shn.shane@gmail.com>
2023-08-08 10:25:12 -0700

    Subs try again

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-"""
-A function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function returns 0.
-"""
-
+"""Function queries subscribers on a given Reddit subreddit."""
 import requests
 
-def number_of_subscribers(subreddit):
-    """
-    If not a valid subreddit, return 0.
-    """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    user_agent = 'User Agent'
-    headers = {
-        'User-Agent': user_agent
-    }
 
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        result = response.json().get('data')
-        return result.get('subscribers')
-    else:
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
